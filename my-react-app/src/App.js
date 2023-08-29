7c0c192a935f37ce3bb928aba1f30f77b75c8ff0
hamzariaz007 <143501588+hamzariaz007@users.noreply.github.com>
2023-08-29 15:20:44 +0500

    Add files via upload

---
+++ my-react-app/src/App.js
@@ -0,0 +1,15 @@
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
+import logo from './logo.svg';
+import './App.css';
+import './style.css';
+
+function App() {
+  return <Home />,
+        <About/>,
+        <Contact/> ;
+   
+}
+
+export default App;
